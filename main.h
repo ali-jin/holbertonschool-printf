de791be77e42e099c2feac528fa2bec4623aba77
MathieuMorel62 <mormth@aol.com>
2022-11-07 05:27:43 -0800

    main file and _putchar file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
