53b30abee2cd45480d756f832b8a28aae867fb5d
root <root@pc>
2022-11-08 14:41:42 +0100

    update

--- get_function.c
+++ get_function.c
@@ -11,32 +11,13 @@
 int get_function(const char *format, print_type argument[], va_list ap)
 {
 	int i = 0;
-<<<<<<< HEAD
-	char *str = format[0], ch;
-
-	while (*str != '\0')
-	{
-		if (*str == '%')
-		{
-			while (argument[i].parameter == NULL)
-			{
-				if (*(argument[i].parameter) == (str + 1))
-				{
-					ch = argument[i].f(format);
-				}
-				i++;
-			}
-		}
-		_putchar(*str);
-		str++;
-=======
 
 	while (argument[i].parameter == NULL)
 	{
 		if (*(argument[i].parameter) == *format)
 			argument[i].f(format);
 		i++;
->>>>>>> dev_alina
 	}
 	return (0);
 }
+
