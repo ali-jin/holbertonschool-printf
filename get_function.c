a7f624eae1a0c61eb21c87ded869c9c508960035
root <root@pc>
2022-11-08 12:02:40 +0100

    code add

--- get_function.c
+++ get_function.c
@@ -9,3 +9,25 @@
  */
 
 int get_function(const char *format, print_type argument[], va_list ap)
+{
+	int i = 0;
+	char *str = format[0], ch;
+
+	while (*str != '\0')
+	{
+		if (*str == '%')
+		{
+			while (argument[i].parameter == NULL)
+			{
+				if (*(argument[i].parameter) == (str + 1))
+				{
+					ch = argument[i].f(format);
+				}
+				i++;
+			}
+		}
+		_putchar(*str);
+		str++;
+	}
+	return (0);
+}
